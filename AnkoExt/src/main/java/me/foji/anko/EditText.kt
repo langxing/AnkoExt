54c4dfdf366ca8d4770534e76b28bc34a9c97b71
chentao <827345030@qq.com>
2017-02-15 13:15:07 +0800

    对扩展进行完善

--- AnkoExt/src/main/java/me/foji/anko/EditText.kt
+++ AnkoExt/src/main/java/me/foji/anko/EditText.kt
@@ -1,6 +1,7 @@
 package me.foji.anko
 
 import android.content.Context
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 
@@ -12,6 +13,10 @@ import android.widget.EditText
  * 显示键盘
  */
 fun EditText.showSoftKeyboard() {
+    isFocusable = true
+    isFocusableInTouchMode = true
+    requestFocus()
+    findFocus()
     val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     imm.showSoftInput(this, InputMethodManager.RESULT_SHOWN)
     imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY)
@@ -23,4 +28,6 @@ fun EditText.showSoftKeyboard() {
 fun EditText.hideSoftKeyboard() {
     val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     imm.hideSoftInputFromWindow(windowToken, 0)
+    (parent as View).isFocusableInTouchMode = true
+    clearFocus()
 }
